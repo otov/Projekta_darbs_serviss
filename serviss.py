e7b25714c3b145ac95496f4b6ee31d299b476086
otov <voicehovicso@gmail.com>
2025-04-03 14:23:47 +0300

    Update serviss.py

---
+++ serviss.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import*
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+logs = Tk()
+logs.geometry("500x500")
+logs.title("Auto servisa kalkulators")
+logs.configure(background="grey")
+
+logs.mainloop()
